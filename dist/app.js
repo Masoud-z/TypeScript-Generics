47d286d7accd5f42b4a901b4bffd8acf07959d13
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-17 17:38:17 +0330

    create generic type for function

--- dist/app.js
+++ dist/app.js
@@ -7,4 +7,13 @@ const promise = new Promise((resolve, reject) => {
 });
 promise.then((data) => {
 });
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const myObj = merge({ name: "Masoud", hobbies: ["sport"] }, { age: 26 });
+console.log(myObj);
+console.log(myObj.name);
+const newObj = merge({ productID: 423423 }, { productName: "Apple" });
+console.log(newObj);
+console.log(newObj.productID);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -14,3 +14,15 @@ promise.then((data) => {
   //   data.split(" ");
   //==> Because we sat that the return value of promise is number we can do string manipulation on its return
 });
+
+//Creating a generic function
+function merge<T extends object, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const myObj = merge({ name: "Masoud", hobbies: ["sport"] }, { age: 26 });
+console.log(myObj);
+console.log(myObj.name);
+const newObj = merge({ productID: 423423 }, { productName: "Apple" });
+console.log(newObj);
+console.log(newObj.productID);
