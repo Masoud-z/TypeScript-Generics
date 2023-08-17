5697aa6dafe70e9bf1b3fd611962fad204c39b11
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-17 17:41:22 +0330

    Add contraints

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ promise.then((data) => {
 });
 
 //Creating a generic function
-function merge<T extends object, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
